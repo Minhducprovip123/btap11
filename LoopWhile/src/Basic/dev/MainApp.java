dfbb72f5e19a753741691309c3ad622b574c86fd
Minhducprovip123 <144077732+Minhducprovip123@users.noreply.github.com>
2023-12-26 22:16:46 +0700

    Add files via upload

---
+++ LoopWhile/src/Basic/dev/MainApp.java
@@ -0,0 +1,107 @@
+package Basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		btap1
+//		int n = 100;
+//		int sum = 0;
+//		int i = 1;
+//
+//		while (i <= n) {
+//			sum += i;
+//			i++;
+//		}
+//
+//		System.out.println("Tong cac so tu 1 den 100: " + sum);
+//
+//	}
+//
+//
+//	}
+////btap2
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("Nhap so tu nhien n: ");
+//		int n = sc.nextInt();
+//
+//		int sum = 0;
+//		int i = 0;
+//
+//		while (i < n) {
+//			if (i % 3 == 0) {
+//				sum += i;
+//			}
+//			i++;
+//		}
+//
+//		System.out.println("Tong cac so nho hon " + n + " va chia het cho 3 la: " + sum);
+//	}
+//
+//}
+//btap3
+//int n = 100;
+//int first = 0, second = 1;
+//int count = 0;
+//
+//System.out.println("100 so Fibonacci dau tien:");
+//
+//while (count < n) {
+//	System.out.print(first + " ");
+//
+//	int next = first + second;
+//	first = second;
+//	second = next;
+//
+//	count++;
+//}
+//}
+//
+//}
+//		btap4
+//		Scanner sc = new Scanner(System.in);
+//
+//		System.out.print("Nhap so nguyen n: ");
+//		int n = sc.nextInt();
+//
+//		int soBanDau = n;
+//		int soDaoNguoc = 0;
+//
+//		while (n != 0) {
+//			int remainder = n % 10;
+//			soDaoNguoc = soDaoNguoc * 10 + remainder;
+//			n /= 10;
+//		}
+//
+//		if (soBanDau == soDaoNguoc) {
+//			System.out.println(soDaoNguoc + " la so Palindrome.");
+//		} else {
+//			System.out.println(soDaoNguoc + " khong phai la so Palindrome.");
+//		}
+//
+//	}
+//		btap5
+//		int n = 100;
+//
+//		while (n <= 999) {
+//			int temp = n;
+//			int sum = 0;
+//
+//			while (temp > 0) {
+//				sum += temp % 10;
+//				temp /= 10;
+//			}
+//
+//			if (sum % 3 == 0) {
+//				System.out.println(n);
+//			}
+//
+//			n++;
+//		}
+//
+//	}
+//}
+
+
